81e3d43c2abd86e1a66cd99b49abc759db786c93
Jen Garcia <jen@haverstack.com>
2022-12-01 21:35:16 -0500

    feat: Implement test requests with mocks

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,26 +1,45 @@
 import fetchAdapter from ".";
 import { AxiosError } from "./types";
 
+// Test URL
+const url = "http://localhost:1";
+
+// Utilities
 const { toString } = Object.prototype;
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+// Mock fetch to avoid making real network requests
+const fetchSpy = jest.spyOn(globalThis, "fetch");
+fetchSpy.mockImplementation(async (input: RequestInfo | URL, init?: RequestInit) => {
+  const req = new Request(input, init);
+  // Simulate network delay
+  await delay(10);
+  if (req.method === "POST") {
+    const url = new URL(req.url);
+    return new Response(JSON.stringify({data: req.body, path: url.pathname}));
+  }
+  return new Response(JSON.stringify({message: "success"}));
+});
 
 test("Regular GET request is successful", async () => {
-  const result = await fetchAdapter({url: "http://example.com"});
+  const result = await fetchAdapter({url});
   expect(result.status).toBe(200);
+  expect(result.data).toBe(JSON.stringify({message: "success"}));
 });
 
 test("Supplying only a base URL creates a successful request", async () => {
-  const result = await fetchAdapter({baseURL: "http://example.com"});
+  const result = await fetchAdapter({baseURL: url});
   expect(result.status).toBe(200);
 });
 
 test("Failing a timeout will throw an error", async () => {
   try {
-    await fetchAdapter({url: "http://example.com", timeout: 1});
+    await fetchAdapter({url, timeout: 1});
     throw new Error("Timeout didn't throw");
   } catch (e: unknown) {
     const error = e as AxiosError;
     expect(error.message).toBe("timeout of 1ms exceeded");
-    expect(error.config?.url).toBe("http://example.com");
+    expect(error.config?.url).toBe(url);
     // Use this opportunity to test JSON output as well
     const errorJson: any = error.toJSON();
     expect(errorJson.message).toBe("timeout of 1ms exceeded");
@@ -30,7 +49,7 @@ test("Failing a timeout will throw an error", async () => {
 test("Set custom timeout message", async () => {
   const timeoutErrorMessage = "YOU TIMED OUT!";
   try {
-    await fetchAdapter({url: "http://example.com", timeout: 1, timeoutErrorMessage});
+    await fetchAdapter({url, timeout: 1, timeoutErrorMessage});
     throw new Error("Timeout didn't throw");
   } catch (e: unknown) {
     const error = e as AxiosError;
@@ -39,38 +58,38 @@ test("Set custom timeout message", async () => {
 });
 
 test("Relative URLs can be combined with a base URL", async () => {
-  const result = await fetchAdapter({baseURL: "http://example.com", url: "/foo"});
-  expect(result.request.url).toBe("http://example.com/foo");
+  const result = await fetchAdapter({baseURL: url, url: "/foo"});
+  expect(result.request.url).toBe(`${url}/foo`);
 });
 
 test("Setting config response types will set response data", async () => {
-  const result1 = await fetchAdapter({url: "http://example.com", responseType: "arraybuffer"});
+  const result1 = await fetchAdapter({url, responseType: "arraybuffer"});
   expect(result1.data instanceof ArrayBuffer).toBe(true);
   expect(toString.call(result1.data)).toBe("[object ArrayBuffer]");
 
-  const result2 = await fetchAdapter({url: "http://example.com", responseType: "blob"});
+  const result2 = await fetchAdapter({url, responseType: "blob"});
   expect(result2.data instanceof Blob).toBe(true);
   expect(toString.call(result2.data)).toBe("[object Blob]");
 
-  const result3 = await fetchAdapter({url: "https://api.artic.edu/api/v1/artworks?limit=1", responseType: "json"});
+  const result3 = await fetchAdapter({url, responseType: "json"});
   expect(result3.data instanceof Object).toBe(true);
-  expect(JSON.stringify(result3.data)).toBeTruthy();
+  expect(result3.data).toEqual({message: "success"});
 });
 
 test("Basic authorization will set request headers", async () => {
   const username = "foo";
   const password = "bar";
-  const result1 = await fetchAdapter({url: "http://example.com", auth: {username, password}});
+  const result1 = await fetchAdapter({url, auth: {username, password}});
   expect(result1.request.headers.get("authorization")).toBe("Basic Zm9vOmJhcg==");
 
-  const result2 = await fetchAdapter({url: "http://example.com", auth: {username: "", password: ""}});
+  const result2 = await fetchAdapter({url, auth: {username: "", password: ""}});
   expect(result2.request.headers.get("authorization")).toBe("Basic Og==");
 });
 
 test("Custom validation can be set for responses", async () => {
   try {
     // Always fail the request
-    await fetchAdapter({url: "http://example.com", validateStatus: (_: number) => {
+    await fetchAdapter({url, validateStatus: (_: number) => {
       return false;
     }});
     throw new Error("Validation didn't throw");
@@ -83,29 +102,31 @@ test("Custom validation can be set for responses", async () => {
   }
 });
 
+test("POST requests send data", async () => {
+  const result = await fetchAdapter({url, method: "post", data: JSON.stringify({foo: "bar"})});
+  expect(result.request.method).toBe('POST');
+  expect(result.request.bodyUsed).toBe(true);
+});
+
+test("Credentials options are set with `withCredentials` option", async () => {
+  // Per the Cloudflare Workers runtime, the `credentials` property won't be
+  // implemented because it doesn't make sense in this environment:
+  // https://github.com/cloudflare/workerd/blob/4e5a05813d4336b15826fd3bda7faedf829c830c/src/workerd/api/http.h#L491-L498
+  const result1 = await fetchAdapter({url, withCredentials: true});
+  expect(result1.status).toBe(200);
+
+  const result2 = await fetchAdapter({url, withCredentials: false});
+  expect(result2.status).toBe(200);
+});
+
 test("Invalid request will throw an error", async () => {
+  // Disables fetch mock for the rest of this file
+  jest.restoreAllMocks();
   try {
-    await fetchAdapter({url: "http://localhost:3000"});
+    await fetchAdapter({url});
     throw new Error("Invalid request didn't throw");
   } catch (e: unknown) {
     const error = e as AxiosError;
     expect(error.message).toBe("Network Error");
   }
 });
-
-test("POST requests send data", async () => {
-  const result = await fetchAdapter({url: "http://example.com", method: "post", data: JSON.stringify({foo: "bar"})});
-  expect(result.request.method).toBe('POST');
-  expect(result.request.bodyUsed).toBe(true);
-});
-
-test("Credentials options are set with `withCredentials` option", async () => {
-  // The `credentials` property of requests is not implemented on service workers...
-  const result1 = await fetchAdapter({url: "http://example.com", withCredentials: true});
-  expect(result1.status).toBe(200);
-
-  const result2 = await fetchAdapter({url: "http://example.com", withCredentials: false});
-  expect(result2.status).toBe(200);
-});
-
-
